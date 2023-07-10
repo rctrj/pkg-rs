b25a6c9649591a2da42d0b1c361ba7f264d8ba71
Rachit Raj <rachitraj2904@gmail.com>
2023-07-10 13:21:42 +0530

    chore: create generic graphql methods

--- src/server/axum/common_routes/graphql.rs
+++ src/server/axum/common_routes/graphql.rs
@@ -1,30 +1,50 @@
-#[macro_export]
-macro_rules! graphql_handler {
-    ($method_name: ident, $schema_name: ident) => {
-        use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-        use axum::extract::Extension;
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig},
+    Schema,
+};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{Html, IntoResponse},
+    Extension,
+};
 
-        pub(crate) async fn $method_name(
-            schema: axum::extract::Extension<$schema_name>,
-            req: GraphQLRequest,
-        ) -> GraphQLResponse {
-            let response = async move { schema.execute(req.into_inner()).await }.await;
-            response.into()
-        }
-    };
+pub async fn graphql_playground(path: &str) -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new(path)))
 }
 
-#[macro_export]
-macro_rules! graphiql_handler {
-    ($path:expr) => {
-        use axum::response::IntoResponse;
+pub async fn graphiql_handler(path: &str) -> impl IntoResponse {
+    Html(
+        async_graphql::http::GraphiQLSource::build()
+            .endpoint(path)
+            .finish(),
+    )
+}
 
-        pub(crate) async fn graphiql() -> impl IntoResponse {
-            axum::response::Html(
-                async_graphql::http::GraphiQLSource::build()
-                    .endpoint($path)
-                    .finish(),
-            )
-        }
-    };
+pub async fn graphql_handler_with_auth<Query, Mutation, Subscription, Token>(
+    schema: Extension<Schema<Query, Mutation, Subscription>>,
+    token: Extension<Token>,
+    req: GraphQLRequest,
+) -> GraphQLResponse
+where
+    Query: async_graphql::ObjectType + 'static,
+    Mutation: async_graphql::ObjectType + 'static,
+    Subscription: async_graphql::SubscriptionType + 'static,
+    Token: Send + Sync + 'static,
+{
+    let mut req = req.into_inner();
+    req = req.data(token.0);
+    schema.execute(req).await.into()
+}
+
+pub async fn graphql_handler<Query, Mutation, Subscription>(
+    schema: Extension<Schema<Query, Mutation, Subscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse
+where
+    Query: async_graphql::ObjectType + 'static,
+    Mutation: async_graphql::ObjectType + 'static,
+    Subscription: async_graphql::SubscriptionType + 'static,
+{
+    let req = req.into_inner();
+    schema.execute(req).await.into()
 }
