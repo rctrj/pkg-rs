647ac221d337a7ef52c4055703ef38c8a8485cba
Rachit Raj <rachitraj2904@gmail.com>
2023-07-10 10:33:00 +0530

    chore: use deadpool instead of r2d2

--- src/errors/conv.rs
+++ src/errors/conv.rs
@@ -24,11 +24,22 @@ map_err!("fast_uaparser", fast_uaparser, fast_uaparser::ParserError);
 map_err!("jwt", jwt, jwt::Error);
 map_err!("mongodb", mongodb, mongodb::error::Error);
 map_err!("phonenumber", phonenumber, phonenumber::ParseError);
-map_err!("r2d2", r2d2, r2d2::Error);
 
 #[cfg(feature = "diesel")]
 pub mod diesel {
     map_err!(diesel::result::Error);
     map_err!(diesel::result::ConnectionError);
     map_err!(diesel::r2d2::Error);
+}
+
+#[cfg(feature = "deadpool")]
+pub mod deadpool {
+    use deadpool::managed::PoolError;
+
+    impl<T> From<PoolError<T>> for super::Error
+    where
+        T: std::error::Error + 'static,
+    {
+        fn from(value: PoolError<T>) -> Self { super::map_err(value) }
+    }
 }
